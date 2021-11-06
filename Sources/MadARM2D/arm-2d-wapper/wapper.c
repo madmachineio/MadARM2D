1a730dad75c55f672cbad0e678baf3f28acfd26d
Frank Li <lgl88911@163.com>
2021-11-06 14:12:05 +0800

    Modify type
    1. modify callback param type
    2. add clang diagnostic
    
    Signed-off-by: Frank Li <lgl88911@163.com>

--- Sources/MadARM2D/arm-2d-wapper/wapper.c
+++ Sources/MadARM2D/arm-2d-wapper/wapper.c
@@ -23,75 +23,92 @@
 #endif
 
 static ARM_NOINIT arm_2d_helper_pfb_t mad_arm_2d_pfb;
-static int (*draw_bitmap)(int,int,int,int,char*) = NULL;
-static int (*draw_bg)(void*, void*) = NULL;
+static int (*draw_bitmap)(int, int, int, int, unsigned char *) = NULL;
+static int (*draw_bg)(void *, void *) = NULL;
 
-static 
-IMPL_PFB_ON_DRAW(__pfb_draw_background_handler)
-{
-    ARM_2D_UNUSED(bIsNewFrame);
+static
+IMPL_PFB_ON_DRAW(__pfb_draw_background_handler){
+	ARM_2D_UNUSED(bIsNewFrame);
 
-    draw_bg(ptTile, pTarget);
+	arm_2d_rgb16_fill_colour(ptTile, NULL, 0xF800);
 
-    return arm_fsm_rt_cpl;
+	arm_2d_region_t tBox = {
+		.tSize = { 200, 100 },
+		.tLocation = { 50, 50 },
+	};
+
+	arm_2d_rgb16_fill_colour(ptTile, &tBox, 0x001F);
+
+	tBox.tLocation.iX -= 10;
+	tBox.tLocation.iY -= 10;
+
+	arm_2d_rgb16_fill_colour(ptTile, &tBox, 0x07E0);
+
+	draw_bg(ptTile, pTarget);
+
+	return arm_fsm_rt_cpl;
 }
 
-static 
-IMPL_PFB_ON_LOW_LV_RENDERING(__pfb_render_handler)
-{
-    const arm_2d_tile_t *ptTile = &(ptPFB->tTile);
+static
+IMPL_PFB_ON_LOW_LV_RENDERING(__pfb_render_handler){
+	const arm_2d_tile_t *ptTile = &(ptPFB->tTile);
 
-    ARM_2D_UNUSED(pTarget);
-    ARM_2D_UNUSED(bIsNewFrame);
+	ARM_2D_UNUSED(pTarget);
+	ARM_2D_UNUSED(bIsNewFrame);
 
-    draw_bitmap(ptTile->tRegion.tLocation.iX,
-                    ptTile->tRegion.tLocation.iY,
-                    ptTile->tRegion.tSize.iWidth,
-                    ptTile->tRegion.tSize.iHeight,
-                    ptTile->pchBuffer);
-                    
-    arm_2d_helper_pfb_report_rendering_complete(&mad_arm_2d_pfb, 
-                                                (arm_2d_pfb_t *)ptPFB);
+	draw_bitmap(ptTile->tRegion.tLocation.iX,
+		    ptTile->tRegion.tLocation.iY,
+		    ptTile->tRegion.tSize.iWidth,
+		    ptTile->tRegion.tSize.iHeight,
+		    ptTile->pchBuffer);
+
+	arm_2d_helper_pfb_report_rendering_complete(&mad_arm_2d_pfb,
+						    (arm_2d_pfb_t *)ptPFB);
 }
 
-void mad_arm_2d_init(int width, 
-                        int height, 
-                        int color_type, 
-                        int pfb_width, 
-                        int pfb_height, 
-                        int (*draw)(int,int,int,int,char*),
-                        int (*bg)(void*,void*))
+void mad_arm_2d_init(int width,
+		     int height,
+		     int color_type,
+		     int pfb_width,
+		     int pfb_height,
+		     int (*draw)(int, int, int, int, unsigned char *),
+		     int (*bg)(void *, void *))
 {
-    arm_2d_init();
+	arm_2d_init();
 
-    __attribute__((section(".bss.noinit.arm_2d_pfb_pool")))                 
-         static struct {                                                        
-            arm_2d_pfb_t tFPB;                                                  
-            __ALIGNED(4)                                                        
-            uint16_t tBuffer[(32) * (32)];                       
-        } s_tPFBs[2];                                                   
-                                                                                
-        arm_2d_helper_pfb_cfg_t tCFG = {                                        
-            .tDisplayArea.tSize = {                                             
-                .iWidth = (width),                                     
-                .iHeight = (height),                                   
-            },                                                                 
-                                                                                
-            .FrameBuffer.ptPFBs = (arm_2d_pfb_t *)s_tPFBs,                      
-            .FrameBuffer.tFrameSize = {                                         
-                .iWidth = (32),                                            
-                .iHeight = (32),                                          
-            },                                                                  
-            .FrameBuffer.wBufferSize = sizeof(s_tPFBs[0].tBuffer),              
-            .FrameBuffer.hwPFBNum = dimof(s_tPFBs),                                                                                  
-        };                                                                     
-                                                                                
-                                     
+	__attribute__((section(".bss.noinit.arm_2d_pfb_pool")))
+	static struct {
+		arm_2d_pfb_t tFPB;
+		__ALIGNED(4)
+		uint16_t tBuffer[(320) * (240)];
+	} s_tPFBs[2];
 
-    if (arm_2d_helper_pfb_init(&mad_arm_2d_pfb, &tCFG) < 0) {
-        //! error detected
-        assert(false);
-    }
+	arm_2d_helper_pfb_cfg_t tCFG = {
+		.tDisplayArea.tSize = {
+			.iWidth = (width),
+			.iHeight = (height),
+		},
 
-    while(arm_fsm_rt_cpl != arm_2d_helper_pfb_task(&mad_arm_2d_pfb,NULL));
+		.FrameBuffer.ptPFBs = (arm_2d_pfb_t *)s_tPFBs,
+		.FrameBuffer.tFrameSize = {
+			.iWidth = (320),
+			.iHeight = (240),
+		},
+		.FrameBuffer.wBufferSize = sizeof(s_tPFBs[0].tBuffer),
+		.FrameBuffer.hwPFBNum = dimof(s_tPFBs),
+	};
+
+
+
+	if (arm_2d_helper_pfb_init(&mad_arm_2d_pfb, &tCFG) < 0) {
+		// ! error detected
+		assert(false);
+	}
+
+	while (arm_fsm_rt_cpl != arm_2d_helper_pfb_task(&mad_arm_2d_pfb, NULL));
+
 }
+
+#if defined(__clang__)
+#   pragma clang diagnostic pop
+#endif
